f5d22f58c213018dd996eca6d5e2fba7d9c35d02
Vladislav Merdineyev <devstardra@gmail.com>
2024-06-07 00:38:03 +0500

    Fix some styles and functions for Drag&Drop functions

--- client/src/components/gameboard/gameboard1.js
+++ client/src/components/gameboard/gameboard1.js
@@ -1,13 +1,15 @@
+'use strict';
+
 import React, { Fragment, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import  { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import { Box, Grid, List, ListItemButton, ListItemText } from '@mui/material';
+import * as ReactDOM from 'react-dom';
+import Dragula from 'react-dragula';
 
-
-const itemLists = ["Derivative Liabilities", "Preferred Stock", "Capitalized Lease Agreement Intangle", "Patent"]
 
 
 const GameDashboard1 = ({
@@ -20,6 +22,10 @@ const GameDashboard1 = ({
   const [selectedIndex, setSelectedIndex] = useState();
   const [tempList1, setTempList1] = useState([]);
 
+  
+  const [itemLists, setItemLists] = useState(["Derivative Liabilities", "Preferred Stock", "Capitalized Lease Agreement Intangle", "Patent"]);
+
+
   useEffect(() => {
     getCurrentProfile();
     // }, []);
@@ -29,23 +35,57 @@ const GameDashboard1 = ({
     e.dataTransfer.setData('text/plain', e.target.textContent);
   };
 
+  const dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = { };
+      Dragula([componentBackingInstance], options);
+    }
+  };
+
+
    //Functions for drop event
-  const handleDrop = (e) => {
+  const handleMainBoardDrop = (e) => {
     e.preventDefault();
     let data = e.dataTransfer.getData('Text/plain');
+    if(data === "") return;
+
     let t = [...tempList1];
     t.push(data);
     setTempList1(t);
 
+    let tmpItemList = [...itemLists]
+    if (tmpItemList.includes(data)) {
+        var index = tmpItemList.indexOf(data);
+        if (index > -1) {
+          tmpItemList.splice(index, 1);
+        }
+    }
+    setItemLists(tmpItemList)
   };
+
+  const handleItemListDrop = (e) => {
+
+      e.preventDefault();
+      let data = e.dataTransfer.getData('Text/plain');
+      if(data === "") return;
+
+      let t = [...itemLists];
+      t.push(data);
+      setItemLists(t);
+
+      let tmpItemList = [...tempList1]
+      if (tmpItemList.includes(data)) {
+          var index = tmpItemList.indexOf(data);
+          if (index > -1) {
+            tmpItemList.splice(index, 1);
+          }
+      }
+      setTempList1(tmpItemList)
+  }
 
   const handleAllowDrop = (e) => {
     e.preventDefault();
   };
-
-  useEffect(() => {
-     console.log(tempList1)
-  }, [tempList1])
 
 
 
@@ -53,19 +93,25 @@ const GameDashboard1 = ({
   return (
     <div> 
        <Grid container spacing={2}>
-       
           <Grid item xs={4}>
-              <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-              <List component="nav" aria-label="main mailbox folders">
-                {itemLists.map((item, index) => (
+              <Box sx={{ width: '100%', maxWidth: 360, minHeight : 600,  bgcolor: 'background.paper' }}   
+                  onDragStart={handleDragStart}
+                  onDragOver={handleAllowDrop}
+                  onDrop={handleItemListDrop}
+                  
+                  >
+                
+              <List component="nav" aria-label="main mailbox folders" className='container' >
+                {(itemLists).map((item, index) => (
                   <ListItemButton
                     key={`item-${item}`}
                     selected={selectedIndex === index}
                     draggable={true}
                     id={item.id}
-                    onDragStart={handleDragStart}
+                  
+                   
                   >
-                    <ListItemText primary={item} />
+                    <ListItemText primary={item}  style={{ border : 3, borderColor : "black",  borderRadius : 5, marging : 3, padding: 5, border : "1px solid black" }}/>
                   </ListItemButton>
                 ))}
               </List>
@@ -74,9 +120,10 @@ const GameDashboard1 = ({
           <Grid item xs={8}>
              <div
                 className="visual-main-panel-screen-content"
-                onDrop={handleDrop}
+                onDragStart={handleDragStart}
+                onDrop={handleMainBoardDrop}
                 onDragOver={handleAllowDrop}
-                style={{width : 250, height : 250, border: 2, borderColor : "black", borderRadius : 5, backgroundColor: "gray"}}
+                style={{width : 250, height : 250, border: 2, borderColor : "black", borderRadius : 5, border : "1px solid black" }}
               >
                   {tempList1.map((item, index) => (
                     <ListItemButton
@@ -85,7 +132,7 @@ const GameDashboard1 = ({
                       draggable={true}
                       id={item}
                     >
-                      <ListItemText primary={item} />
+                      <ListItemText primary={item} style={{ border : 3, borderColor : "black",  borderRadius : 5, padding: 5,  border : "1px solid black"  }}/>
                   </ListItemButton>
                 ))}
               </div>
@@ -93,6 +140,8 @@ const GameDashboard1 = ({
       </Grid>
     </div>
   )
+
+
 
 };
 
