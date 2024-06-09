868fbcb9436995f3c8dd94a225ecea68f485b9e6
Vladislav Merdineyev <devstardra@gmail.com>
2024-06-09 23:50:42 +0500

    dragdrop design, tooltip

--- client/src/components/gameboard/gameboard1.js
+++ client/src/components/gameboard/gameboard1.js
@@ -1,24 +1,48 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Tooltip from '@mui/material/Tooltip';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 const initialItems = {
   account: [
-    { id: 'item1', content: 'Accounts Payable' },
-    { id: 'item2', content: 'Furniture & Fixtures' },
-    { id: 'item3', content: 'Allowance of Bad Debts' },
-    { id: 'item4', content: "Owner's Investment" },
+    { id: 'item1', content: 'Accounts Payable', answer: 'currentLiabilities', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item2', content: 'Furniture & Fixtures', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item3', content: 'Allowance of Bad Debts', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item4', content: "Owner's Investment", answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item5', content: 'Franchise Agreements', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item6', content: 'Joint Ventures', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item7', content: 'Virtual Certificates', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item8', content: "Customer Deposits", answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item9', content: 'Gift Cards & Loyalty Points', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
+    { id: 'item10', content: 'Capitalized Research & Development', answer: 'propertyPlantEquipment', tooltip: "This is a tooltip to describe the accounts" },
   ],
   currentAssets: [],
   propertyPlantEquipment: [],
-  intangibleOrFinancialAssets: [],
+  intangibleAssets: [],
+  financialAssets: [],
+  digitalAssets: [],
   currentLiabilities: [],
   longTermLiabilities: [],
   accruals: [],
   capital: [],
 };
+
+const assetCategories = ["currentAssets", "propertyPlantEquipment", "intangibleAssets", "financialAssets", "digitalAssets"]
+const liabilitiesCategories = ["currentLiabilities", "longTermLiabilities", "accruals", "capital"]
+
+const categoryNames = {
+  currentAssets: "Current Assets",
+  propertyPlantEquipment: "Property, Plant & Equipment",
+  intangibleAssets: "Intangible Assets",
+  financialAssets: "Financial Assets",
+  digitalAssets: "Digital Assets",
+  currentLiabilities: "Current Liabilities",
+  longTermLiabilities: "Long-Term Liabilities",
+  accruals: "Accruals",
+  capital: "Capital"
+}
 
 const GameDashboard1 = ({
   getCurrentProfile,
@@ -50,29 +74,76 @@ const GameDashboard1 = ({
   };
 
   return (
-    <div className="container mx-auto flex justify-between h-full py-10">
+    <div className="mx-auto flex justify-between h-full py-10">
       <DragDropContext onDragEnd={handleDragEnd}>
-        {Object.keys(state).map((droppableId, i) => (
-          <Droppable key={droppableId} droppableId={droppableId}>
+        <div className="flex flex-col items-center p-4 pt-7 rounded-lg">
+          <h2 className="mb-4 uppercase font-bold text-xl text-gray-800">Account</h2>
+          <div className="h-7" />
+          <Droppable droppableId="account">
             {(provided, snapshot) => (
-              <div className="flex flex-col items-center">
-                <h2 className="mb-2 uppercase font-bold text-lg">{droppableId}</h2>
-                <div ref={provided.innerRef} {...provided.droppableProps} className="border-2 border-gray-200 rounded w-64 min-h-3 p-2">
-                  {state[droppableId].map((item, index) => (
-                    <Draggable key={item.id} draggableId={item.id} index={index}>
-                      {(provided, snapshot) => (
-                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="border-2 border-gray-300 rounded my-1 p-2">
-                          {item.content}
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
+              <div ref={provided.innerRef} {...provided.droppableProps} className="border-2 border-gray-200 bg-white rounded-lg w-96 min-h-96 p-1 overflow-auto">
+                {state.account.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <Tooltip title = {item.tooltip} arrow><div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="border-2 border-gray-300 bg-gray-100 rounded-lg m-1 p-1 shadow-md flex items-center justify-center h-12 font-sans">
+                        {item.content}
+                      </div></Tooltip>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
               </div>
             )}
           </Droppable>
-        ))}
+        </div>
+        <div className="flex flex-col items-center bg-blue-50 p-4 rounded-lg shadow-md">  
+          <h2 className="mb-4 uppercase font-bold text-xl text-blue-800">Debt balance</h2>
+          {assetCategories.map((droppableId) => (
+            <Droppable key={droppableId} droppableId={droppableId}>
+              {(provided, snapshot) => (
+                <div className="flex flex-col items-center mt-4">
+                  <h3 className="mb-2 font-bold text-md text-blue-600">{categoryNames[droppableId]}</h3>
+                  <div ref={provided.innerRef} {...provided.droppableProps} className="border-2 border-blue-200 bg-white rounded-lg w-96 min-h-20 max-h-48 p-1 mb-4 shadow overflow-auto">
+                    {state[droppableId].map((item, index) => (
+                      <Draggable key={item.id} draggableId={item.id} index={index}>
+                        {(provided, snapshot) => (
+                          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="border-2 border-gray-300 bg-gray-100 rounded-lg m-1 p-1 shadow-md flex items-center justify-center h-12 font-sans">
+                            {item.content}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
+        <div className="flex flex-col items-center bg-green-50 p-4 rounded-lg shadow-md">
+          <h2 className="mb-4 uppercase font-bold text-xl text-green-800">Credit balance</h2>
+          {liabilitiesCategories.map((droppableId) => (
+            <Droppable key={droppableId} droppableId={droppableId}>
+              {(provided, snapshot) => (
+                <div className="flex flex-col items-center mt-4">
+                  <h3 className="mb-2 font-bold text-md text-green-600">{categoryNames[droppableId]}</h3>
+                  <div ref={provided.innerRef} {...provided.droppableProps} className="border-2 border-green-200 bg-white rounded-lg w-96 min-h-20 max-h-48 p-1 mb-4 shadow overflow-auto">
+                    {state[droppableId].map((item, index) => (
+                      <Draggable key={item.id} draggableId={item.id} index={index}>
+                        {(provided, snapshot) => (
+                          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="border-2 border-gray-300 bg-gray-100 rounded-lg m-1 p-1 shadow-md flex items-center justify-center h-12 font-sans">
+                            {item.content}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
       </DragDropContext>
     </div>
   );
