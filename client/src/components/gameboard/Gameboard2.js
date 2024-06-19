5a01db265b09284215cc66013c9ef09d16bd3523
Vladislav Merdineyev <devstardra@gmail.com>
2024-06-20 02:53:13 +0500

    number format solved

--- client/src/components/gameboard/Gameboard2.js
+++ client/src/components/gameboard/Gameboard2.js
@@ -18,6 +18,7 @@ import { TAccount, TArray } from '../../models/TAccounting';
 import { debitCategories, creditCategories, categoryNames } from './Constants';
 import ScoreboardItem from './ScoreboardItem';
 import BVAInput from './BVAInput';
+import zIndex from '@mui/material/styles/zIndex';
 
 const GameDashboard1 = ({
   auth: { user },
@@ -213,7 +214,7 @@ const GameDashboard1 = ({
                   <div className="flex flex-col items-center">
                     <div ref={provided.innerRef} {...provided.droppableProps} className="border-2 border-green-200 bg-white rounded-lg w-72 h-8 mb-1 shadow">
                       {boxes[TAccount.boxName]?.map((item, index) => (
-                        <Draggable key={item._id} draggableId={item._id} index={index}>
+                        <Draggable key={item._id} draggableId={item._id} index={index} style={{ zIndex: 1000 }}>
                           {(provided, snapshot) => (
                             <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="border-2 border-gray-300 bg-gray-100 rounded-lg shadow-md flex items-center justify-center h-8 font-sans font-semibold">
                               {item.name}
@@ -228,10 +229,10 @@ const GameDashboard1 = ({
               </Droppable>
               <Box sx={{ borderTop: 3, borderColor: 'black', width: '18rem', mt: 1, pb: 2, display: 'flex', flexDirection: 'row' }}>
                 <Box sx={{ borderRight: 3, borderColor: 'black', flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <TextField size="small" sx={{ width: '8rem', bgcolor: 'white', mt: 1 }} />
+                  <BVAInput />
                 </Box>
                 <Box sx={{ borderColor: 'divider', pb: 2, flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <TextField size="small" sx={{ width: '8rem', bgcolor: 'white', mt: 1 }} />
+                  <BVAInput />
                 </Box>
               </Box>
             </div>
@@ -261,10 +262,10 @@ const GameDashboard1 = ({
               </Droppable>
               <Box sx={{ borderTop: 3, borderColor: 'black', width: '18rem', mt: 1, pb: 2, display: 'flex', flexDirection: 'row' }}>
                 <Box sx={{ borderRight: 3, borderColor: 'black', flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <TextField size="small" sx={{ width: '8rem', bgcolor: 'white', mt: 1 }} />
+                  <BVAInput />
                 </Box>
                 <Box sx={{ borderColor: 'divider', pb: 2, flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <TextField size="small" sx={{ width: '8rem', bgcolor: 'white', mt: 1 }} />
+                  <BVAInput />
                 </Box>
               </Box>
             </div>
